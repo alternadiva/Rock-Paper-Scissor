ba794f81e2baf7b57c0758674a9bd05054482cdd
alternadiva <asomeonelse@gmail.com>
2022-02-23 22:36:11 +0100

    create function with  rock-paper-scissor algorithm

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-
+function rPS(player1, player2) {
+  const outputMessage = "The winner is ";
+  if (player1 !== player2) {
+    if (player1 === "rock" && player2 === "scissor") {
+      return outputMessage + player1;
+    } else if (player1 === "scissor" && player2 === "paper") {
+      return outputMessage + player1;
+    } else if (player1 === "paper" && player2 === "rock") {
+      return outputMessage + player1;
+    } else {
+      return outputMessage + player2;
+    }
+  } else {
+    return "Draw";
+  }
+}
